14d598ab474ad04c894e02a48c7a0f0920428669
Jason Serafica <seraficaj@gmail.com>
2024-02-15 11:27:51 -0800

    complete frontEgg onboarding. integrate login, sign-up and admin portal

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { FronteggProvider } from "@frontegg/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const contextOptions = {
+    baseUrl: "https://app-dxjx03vmag6q.frontegg.com",
+    clientId: "4004bae9-21db-4511-8f5a-d93ad4f6fad1",
+};
+
+const authOptions = {
+    // keepSessionAlive: true // Uncomment this in order to maintain the session alive
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <FronteggProvider
+            contextOptions={contextOptions}
+            hostedLoginBox={true}
+            authOptions={authOptions}
+        >
+            <App />
+        </FronteggProvider>
+    </React.StrictMode>
+);
